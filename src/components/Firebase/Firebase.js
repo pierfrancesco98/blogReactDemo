a41c58c8569825ea7d9d9aad222f2df7cdbffb4f
Pierfrancesco Pisani <109861869+pierfrancesco98@users.noreply.github.com>
2023-08-24 12:01:47 +0200

    Update Firebase.js

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -4,13 +4,13 @@ import { getDatabase, set, ref, update, push, get, child, remove, onValue } from
 import { getStorage, uploadBytes, getDownloadURL,  ref as sRef } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnfsfxfG8TpyhqCRfMxUtf8NqcfJlLeQs",
-  authDomain: "blogreactdemos.firebaseapp.com",
-  databaseURL: "https://blogreactdemos-default-rtdb.firebaseio.com",
-  projectId: "blogreactdemos",
-  storageBucket: "blogreactdemos.appspot.com",
-  messagingSenderId: "172090635102",
-  appId: "1:172090635102:web:fbcd3a28c2e7d1304295cf"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
